5bbce50278173048bffb7911b61bf4aad079b67f
Istvan Soos <istvan.soos@agilord.com>
2017-03-08 23:39:58 +0100

    Update license.

--- lib/aws_client.dart
+++ lib/aws_client.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2016, project contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
 import 'package:http_client/http_client.dart';
 
 import 'src/credentials.dart';
--- lib/sqs.dart
+++ lib/sqs.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2016, project contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
 import 'dart:async';
 
 import 'src/request.dart';
