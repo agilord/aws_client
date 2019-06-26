0c989e07e87ada6131b5096bc733525e5af4f5a4
压力很大同志 <superpowerlee@gmail.com>
2019-06-26 14:04:32 +0800

    add create queue api (#8)
    * add create queue api
    
    * add a new line after last line
    
    * fix issues
    
    * use package:meta to make named parameter required

---
+++ test/impl_test.dart
@@ -0,0 +1,34 @@
+library aws_client.test.impl_test;
+
+import 'dart:io';
+import "package:test/test.dart";
+import 'package:aws_client/aws_client.dart';
+import 'package:aws_client/sqs.dart';
+import 'package:http_client/console.dart';
+
+void main() {
+
+  Map<String, String> env = Platform.environment;
+
+  group('SQS-queue', (){
+
+    test('create-queue', () async {
+      final httpClient = new ConsoleClient();
+      final credentials = new Credentials(accessKey: env['AWS_ACCESS_KEY_ID'], secretKey: env['AWS_SECRET_ACCESS_KEY']);
+      final aws = new Aws(credentials: credentials, httpClient: httpClient);
+      final queue = await aws.sqs.create(region:env['AWS_DEFAULT_REGION'], queueName:'[set your queue name]');
+      await queue.sendMessage('test message');
+      expect(true, equals(true));
+    });
+
+    test('Queue-send', () async {
+      final httpClient = new ConsoleClient();
+      final credentials = new Credentials(accessKey: env['AWS_ACCESS_KEY_ID'], secretKey: env['AWS_SECRET_ACCESS_KEY']);
+      final aws = new Aws(credentials: credentials, httpClient: httpClient);
+      final queue = aws.sqs.queue('[set your own]');
+      await queue.sendMessage('test test test');
+    });
+
+  });
+
+}
