cd03996819bf3cae66269826906cebabfdb69074
Istvan Soos <istvan.soos@agilord.com>
2019-12-14 18:27:39 +0100

    Fix CI on Dart SDK 2.7

--- aws_client/lib/src/scoping_extensions.dart
+++ aws_client/lib/src/scoping_extensions.dart
@@ -1,12 +1,12 @@
 extension ScopingFunctions<T> on T {
   /// Calls the specified function [block] with `this` value
   /// as its argument and returns its result.
-  R let<R>(R Function(T) block) => block(this as T);
+  R let<R>(R Function(T) block) => block(this);
 
   /// Calls the specified function [block] with `this` value
   /// as its argument and returns `this` value.
   T also(void Function(T) block) {
-    block(this as T);
-    return this as T;
+    block(this);
+    return this;
   }
 }
