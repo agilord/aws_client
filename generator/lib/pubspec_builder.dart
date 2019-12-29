5b0b20e6d1aa76d0c7f3ced0cf0de3bcca8a3e16
Istvan Soos <istvan.soos@agilord.com>
2019-12-29 15:16:29 +0100

    YAML-formatted pubspec.yaml

---
+++ generator/lib/pubspec_builder.dart
@@ -0,0 +1,34 @@
+import 'package:meta/meta.dart';
+
+import 'model/api.dart';
+
+String buildPubspecYaml(
+  Api api, {
+  @required String packageVersion,
+  @required String sharedVersion,
+  @required bool isDevMode,
+}) {
+  final override = isDevMode ? '''
+dependency_overrides:
+  aws_client:
+    path: ../../aws_client
+''': '';
+
+  return '''name: ${api.packageName}
+version: $packageVersion
+publish_to: none
+
+environment:
+  sdk: '>=2.6.0 <3.0.0'
+
+dependencies:
+  aws_client: $sharedVersion
+  http: ^0.12.0
+  json_annotation: ^3.0.0
+
+dev_dependencies:
+  build_runner: ^1.7.2
+  json_serializable: ^3.2.0
+$override
+''';
+}
