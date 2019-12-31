c2aa089ca530a7886fea6c412f697d514e2c943f
Jonathan Böcker <jonathan.bocker1@ingka.com>
2019-12-31 10:19:57 +0100

    Boilerplate for json protocol

--- aws_client/lib/src/protocol/json.dart
+++ aws_client/lib/src/protocol/json.dart
@@ -1,13 +1,38 @@
+// ignore_for_file: unused_field
 import 'package:http/http.dart';
+
+import '../credentials.dart';
+import 'shared.dart';
 
 class JsonProtocol {
   final String _endpointUrl;
   final Client _client;
+  final String _service;
+  final String _region;
+  final Credentials _credentials;
 
-  JsonProtocol({String endpointUrl, Client client})
-      : _endpointUrl = endpointUrl,
-        _client = client ?? Client() {
-    print(_endpointUrl);
-    print(_client);
+  JsonProtocol._(
+    this._client,
+    this._service,
+    this._region,
+    this._endpointUrl,
+    this._credentials,
+  );
+
+  factory JsonProtocol({
+    Client client,
+    String service,
+    String region,
+    String endpointUrl,
+    Credentials credentials,
+  }) {
+    client ??= Client();
+    if (service == null || region == null) {
+      ArgumentError.checkNotNull(endpointUrl, 'endpointUrl');
+    }
+    endpointUrl ??= 'https://$service.$region.amazonaws.com';
+    service ??= extractService(Uri.parse(endpointUrl));
+    region ??= extractRegion(Uri.parse(endpointUrl));
+    return JsonProtocol._(client, service, region, endpointUrl, credentials);
   }
 }
