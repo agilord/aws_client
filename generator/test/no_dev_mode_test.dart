0cfbeeb7904d6d4e636677474631c828b9c2317e
Istvan Soos <istvan.soos@agilord.com>
2020-01-04 19:48:42 +0100

    Use foreach

--- generator/test/no_dev_mode_test.dart
+++ generator/test/no_dev_mode_test.dart
@@ -9,16 +9,17 @@ void main() {
   test('Generated pubspec.yaml should not have dev mode artifacts.', () async {
     final generatedDir = Directory('../generated');
     await generatedDir.create(recursive: true);
-    final pubspecFiles = await generatedDir
+    await generatedDir
         .list(recursive: true)
         .where((f) => f is File)
         .cast<File>()
         .where((f) => basename(f.path) == 'pubspec.yaml')
-        .toList();
-    for (final file in pubspecFiles) {
-      final content = await file.readAsString();
-      expect(content, isNot(contains('dependency_overrides')));
-      expect(content, isNot(contains('../../shared_aws_api')));
-    }
+        .forEach(
+      (file) {
+        final content = file.readAsStringSync();
+        expect(content, isNot(contains('dependency_overrides')));
+        expect(content, isNot(contains('../../shared_aws_api')));
+      },
+    );
   });
 }
