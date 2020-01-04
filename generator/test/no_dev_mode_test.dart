ee1506c71745749424a250288545487a93e16e55
Istvan Soos <istvan.soos@agilord.com>
2020-01-04 19:18:11 +0100

    Protect against checking in dev mode artifacts.

---
+++ generator/test/no_dev_mode_test.dart
@@ -0,0 +1,22 @@
+@Tags(['presubmit-only'])
+
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Generated pubspec.yaml should not have dev mode artifacts.', () async {
+    final pubspecFiles = await Directory('../generated')
+        .list(recursive: true)
+        .where((f) => f is File)
+        .cast<File>()
+        .where((f) => basename(f.path) == 'pubspec.yaml')
+        .toList();
+    for (final file in pubspecFiles) {
+      final content = await file.readAsString();
+      expect(content, isNot(contains('dependency_overrides')));
+      expect(content, isNot(contains('../../shared_aws_api')));
+    }
+  });
+}
