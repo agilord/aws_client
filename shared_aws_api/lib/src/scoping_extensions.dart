f5c178dcd143b3e35f34b6069f5b6cce3f121719
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2020-05-24 21:58:18 +0200

    Add rest of methods to DocumentClient (#185)

--- shared_aws_api/lib/src/scoping_extensions.dart
+++ shared_aws_api/lib/src/scoping_extensions.dart
@@ -1,11 +1,11 @@
 extension ScopingFunctions<T> on T {
   /// Calls the specified function [block] with `this` value
   /// as its argument and returns its result.
-  R let<R>(R Function(T) block) => block(this);
+  R let<R>(R Function(T e) block) => block(this);
 
   /// Calls the specified function [block] with `this` value
   /// as its argument and returns `this` value.
-  T also(void Function(T) block) {
+  T also(void Function(T e) block) {
     block(this);
     return this;
   }
