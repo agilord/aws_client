606e8ee88596721b483d57a85b2bc229d914f4ff
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2020-06-02 10:45:58 +0200

    Update the example template, to reflect automatic credential resolving (#205)

--- generator/lib/builders/example_builder.dart
+++ generator/lib/builders/example_builder.dart
@@ -5,8 +5,7 @@ String buildExampleReadme(Api api) => '''
 import 'package:${api.packageName}/${api.fileBasename}.dart';
 
 void main() {
-  final credentials = AwsClientCredentials(accessKey: '', secretKey: '');
-  final service = ${api.metadata.className}(region: 'eu-west-1', credentials: credentials);
+  final service = ${api.metadata.className}(region: 'eu-west-1');
   // See documentation on how to use ${api.metadata.className}
 }
 ```
