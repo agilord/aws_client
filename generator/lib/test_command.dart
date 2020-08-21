7e6fc7b67af5bcbab0a8ffc57adbe8a9acb5586b
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2020-08-21 18:29:08 +0200

    parallelize test command

--- generator/lib/test_command.dart
+++ generator/lib/test_command.dart
@@ -11,15 +11,15 @@ class TestCommand extends Command {
 
   @override
   void run() async {
-    final packages = Directory('../generated').listSync();
+    final packages =
+        Directory('../generated').listSync().map((e) => e.path).toList();
     print('Running tests in generated packages.');
-    final length = packages.length;
-    for (var i = 0; i < length; i++) {
-      final package = packages[i];
-      print('$i/$length ${package.path}');
-      await _getDependencies(package.path);
-      await _runTests(package.path);
-    }
+
+    List.generate(
+        (Platform.numberOfProcessors - 2)
+            .clamp(1, Platform.numberOfProcessors)
+            .toInt(),
+        (index) async => _runTests(packages));
   }
 
   Future<void> _getDependencies(String baseDir, {bool upgrade = true}) async {
@@ -39,19 +39,28 @@ class TestCommand extends Command {
     }
   }
 
-  Future<void> _runTests(String baseDir) async {
-    final pr = await Process.run(
-      'pub',
-      [
-        'run',
-        'test',
-      ],
-      workingDirectory: baseDir,
-    );
-    if (pr.exitCode != 0) {
-      print(pr.stdout);
-      print(pr.stderr);
-      throw Exception('pub run test failed at $baseDir');
+  Future<void> _runTests(List<String> dirs) async {
+    while (dirs.isNotEmpty) {
+      final baseDir = dirs.removeLast();
+      final dirsLeft = dirs.length;
+
+      print('Running tests in $baseDir, dirs left: $dirsLeft');
+
+      await _getDependencies(baseDir);
+
+      final pr = await Process.run(
+        'pub',
+        [
+          'run',
+          'test',
+        ],
+        workingDirectory: baseDir,
+      );
+      if (pr.exitCode != 0) {
+        print(pr.stdout);
+        print(pr.stderr);
+        throw Exception('pub run test failed at $baseDir');
+      }
     }
   }
 }
