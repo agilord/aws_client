b88bc2d8e690615bc38e647b662f1785c357ecd3
JihwanKim <jihwan.dev@gmail.com>
2022-08-01 01:22:40 +0900

    Add endpointUrl element to Query Builder constructor

--- generator/lib/builders/protocols/query_builder.dart
+++ generator/lib/builders/protocols/query_builder.dart
@@ -20,7 +20,7 @@ class QueryServiceBuilder extends ServiceBuilder {
     ${isRegionRequired ? 'required String' : 'String?'} region,
     _s.AwsClientCredentials? credentials,
     _s.AwsClientCredentialsProvider? credentialsProvider,
-    _s.Client? client,
+    _s.Client? client, String? endpointUrl,
     })
   : _protocol = _s.QueryProtocol(
       client: client,
@@ -28,6 +28,7 @@ class QueryServiceBuilder extends ServiceBuilder {
       region: region,
       credentials: credentials,
       credentialsProvider: credentialsProvider,
+      endpointUrl: endpointUrl,
     ),
   shapes = shapesJson.map((key, value) => MapEntry(key, _s.Shape.fromJson(value)));
   ''';
