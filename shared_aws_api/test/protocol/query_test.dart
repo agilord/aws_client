17143e0d210a0a84ed4637e96a716cdd0d9d12f1
Chuck Batson <cbatson3@gmail.com>
2022-12-02 12:08:37 -0930

    stronger test

--- shared_aws_api/test/protocol/query_test.dart
+++ shared_aws_api/test/protocol/query_test.dart
@@ -6,5 +6,10 @@ void main() {
     test('handles a value of type double', () {
       expect(() => flatQueryParams(0.0, null, {}, 'version', 'action'), returnsNormally);
     });
+
+    test('correct output for a value of type double', () {
+      final result = flatQueryParams(123.4, null, {}, 'version', 'action');
+      expect(result[''], '123.4');
+    });
   });
 }
