6f5833ff136469a4d5f96aef7605fe6675502d76
Xavier H <xavier.hainaux@gmail.com>
2023-06-22 23:12:02 +0200

    Put code in toplevel shared_aws_api package (#401)

--- aws_client/lib/src/shared/src/utils/query_string.dart
+++ aws_client/lib/src/shared/src/utils/query_string.dart
@@ -4,11 +4,13 @@ String canonicalQueryParameters(Map<String, String> query) {
 }
 
 String canonicalQueryParametersAll(Map<String, List<String>> query) {
+  String encodeComponent(String input) =>
+      Uri.encodeQueryComponent(input).replaceAll('+', '%20');
+
   final items = <String>[];
   for (var key in query.keys) {
     for (var value in query[key]!) {
-      items.add(
-          '${Uri.encodeQueryComponent(key).replaceAll('+', '%20')}=${Uri.encodeQueryComponent(value).replaceAll('+', '%20')}');
+      items.add('${encodeComponent(key)}=${encodeComponent(value)}');
     }
   }
   items.sort();
--- shared_aws_api/lib/src/utils/query_string.dart
+++ shared_aws_api/lib/src/utils/query_string.dart
@@ -4,11 +4,13 @@ String canonicalQueryParameters(Map<String, String> query) {
 }
 
 String canonicalQueryParametersAll(Map<String, List<String>> query) {
+  String encodeComponent(String input) =>
+      Uri.encodeQueryComponent(input).replaceAll('+', '%20');
+
   final items = <String>[];
   for (var key in query.keys) {
     for (var value in query[key]!) {
-      items.add(
-          '${Uri.encodeQueryComponent(key)}=${Uri.encodeQueryComponent(value)}');
+      items.add('${encodeComponent(key)}=${encodeComponent(value)}');
     }
   }
   items.sort();
